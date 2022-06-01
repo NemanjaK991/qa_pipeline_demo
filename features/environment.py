f397d3d6c1356b15dc734012df97d114b8078ba4
Nemanja Koricic <nemanja.koricic@htecgroup.com>
2022-06-01 13:32:31 +0200

    Adding additional argument options

--- features/environment.py
+++ features/environment.py
@@ -14,6 +14,8 @@ if configuration.headless_mode:
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 else:
     options = None
 
