1fe480b2743457c3741a734ae0d8ee704bda6a43
Nemanja Koricic <nemanja.koricic@htecgroup.com>
2022-06-01 15:35:29 +0200

    delete no-sandbox

--- features/environment.py
+++ features/environment.py
@@ -15,7 +15,6 @@ if configuration.headless_mode:
     options.add_argument("--no-sandbox")
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 else:
     options = None
